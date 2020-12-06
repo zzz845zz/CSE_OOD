492faabad85793c35b1eed5591bd8ee44f93605b
SeungminJeon <zzz845zz@gmail.com>
2020-12-06 15:05:24 +0900

    Termp: in progress

--- Term/include/CommandIntepreter.hpp
+++ Term/include/CommandIntepreter.hpp
@@ -94,28 +94,8 @@ class CommandIntepreter {
         }
 
     public:
-        CommandIntepreter() {
-            // [cargs] in [Command] is empty at the first time.
-            // It will be filled in `fill_cargs()` 
-            vector<Command*> vec;
-            vec.push_back(new CommandInsert());
-            vec.push_back(new CommandDelete());
-            vec.push_back(new CommandSearch());
-            vec.push_back(new CommandReplace());
-            vec.push_back(new CommandTerminate());
-            vec.push_back(new CommandNextPage());
-            vec.push_back(new CommandPreviousPage());
-
-            for (auto it = vec.begin(); it!=vec.end(); ++it) {
-                cmd_map[(*it)->symbol] = *it;
-            }
-        }
-        ~CommandIntepreter() {
-            // Deallocate all [Command] allocated in constructor
-            for(auto it = cmd_map.begin(); it!=cmd_map.end(); ++it) {
-                delete(it->second); // delete value
-            }
-        }
+        CommandIntepreter();
+        ~CommandIntepreter();
 
         // Get single global instance
         static CommandIntepreter* instance() {
--- Term/src/CommandIntepreter.cpp
+++ Term/src/CommandIntepreter.cpp
@@ -2,6 +2,30 @@
 
 using namespace std;
 
+CommandIntepreter::CommandIntepreter() {
+    // [cargs] in [Command] is empty at the first time.
+    // It will be filled in `fill_cargs()` 
+    vector<Command*> vec;
+    vec.push_back(new CommandInsert());
+    vec.push_back(new CommandDelete());
+    vec.push_back(new CommandSearch());
+    vec.push_back(new CommandReplace());
+    vec.push_back(new CommandTerminate());
+    vec.push_back(new CommandNextPage());
+    vec.push_back(new CommandPreviousPage());
+
+    for (auto it = vec.begin(); it!=vec.end(); ++it) {
+        cmd_map[(*it)->symbol] = *it;
+    }
+}
+
+CommandIntepreter::~CommandIntepreter() {
+    // Deallocate all [Command] allocated in constructor
+    for(auto it = cmd_map.begin(); it!=cmd_map.end(); ++it) {
+        delete(it->second); // delete value
+    }
+}
+
 Command* CommandIntepreter::interptret_input(string input) {
     Command* cmd = get_command_ptr(input.at(0));
 
